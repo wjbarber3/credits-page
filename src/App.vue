d7691e22b62e03be605eb6fdf0574914a8c31b6e
Jordan Barber <wjbarber3@gmail.com>
2018-10-29 12:27:10 -0600

    adding global variables and typography sass files, adding parent Vue component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
   <div id="app">
+    <credit-list></credit-list>
   </div>
 </template>
 
 <script>
 
+import CreditList from './components/CreditList.vue';
+
 export default {
   name: 'app',
+  components: {
+    CreditList
+  },
   data () {
     return {
+      headline: 'Stock up on Credits',
+      subhead: 'Save money and get free bonus credits when you buy in bulk'
     }
   }
 }
 </script>
 
 <style lang="scss">
+
+  @import 'styles/typography.scss';
+  @import 'styles/variables.scss';
+
+  body {
+    background: $light_grey;
+    padding-top: 43px;
+  }
 
 </style>
---
+++ src/components/CreditList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <h2>{{ tagline }}</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'BookList',
+  data() {
+    return {
+      title: 'Stock up on Credits',
+      tagline: 'Save money and get free bonus credits when you buy in bulk',
+      creditOptions: [],
+    }
+  }
+}
+
+</script>
+
+<style lang="scss">
+</style>
