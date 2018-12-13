62a8c97e7855c0c1216a2a10c4697c2b2e25caa1
Jordan Barber <wjbarber3@gmail.com>
2018-12-12 21:33:50 -0700

    adding constansts

---
+++ src/constants/paymentTypes.js
@@ -0,0 +1,4 @@
+export default Object.freeze({
+  CreditCard: 'Credit Card',
+  PayPal: 'PayPal'
+})
---
+++ src/constants/successMessages.js
@@ -0,0 +1,5 @@
+export default Object.freeze({
+  CreditSuccess: 'Woohoo! You received credits for $',
+  PayPalSuccess: 'Woohoo! Off To Paypal for $',
+  WithBonus: '. Plus a bonus of $'
+})
